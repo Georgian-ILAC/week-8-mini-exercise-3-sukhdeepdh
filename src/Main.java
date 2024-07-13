a07eec413c0ac3c81a4ca591d44e78b54148c102
sukhdeepdh <sukhdeepdh24@gmail.com>
2024-07-13 09:47:05 -0400

    Assignmnet completed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Employee emp = new Employee("Sukhdeep", "Kaur", 3111, 19.5, 6.0);
+        // Ensure you are providing all the required arguments
+        // with correct types and in the right order.
+
+        emp.setHeight(5.7);
+        System.out.println(emp);
+        System.out.println("Hourly Pay after raise: $" + emp.getRaise());
+        int hoursWorked = 45;
+        System.out.println("Total pay for " + hoursWorked + " hours: $" + emp.payDay(hoursWorked));
     }
 }
--- src/Person.java
+++ src/Person.java
@@ -1,6 +1,6 @@
 public class Person {
-    private  String firstName;
-    private  String lastName;
+    private String firstName;
+    private String lastName;
     private double height;
 
     public Person(String firstName, String lastName) {
@@ -16,20 +16,70 @@ public class Person {
         return lastName;
     }
 
-    private double getHeight(){
+    protected double getHeight() {  // Changed from private to protected
         return height;
     }
 
-    public void setFirstName(String firstName){
+    public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
 
-    public void setLastName(String lastName){
+    public void setLastName(String lastName) {
         this.lastName = lastName;
     }
 
-    private void setHeight(double height){
-        this.height=height;
+    public void setHeight(double height) {  // Changed from private to public
+        this.height = height;
+    }
+}
+
+class Employee extends Person {
+    private int id;
+    private double hourlyPay;
+
+    public Employee(String firstName, String lastName, int id, double hourlyPay, double height) {
+        super(firstName, lastName);
+        this.id = id;
+        this.hourlyPay = hourlyPay;
+
     }
 
+    // Getter and Setter for id
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    // Getter and Setter for hourlyPay
+    public double getHourlyPay() {
+        return hourlyPay;
+    }
+
+    public void setHourlyPay(double hourlyPay) {
+        this.hourlyPay = hourlyPay;
+    }
+
+    // Method to give a raise
+    public double getRaise() {
+        this.hourlyPay *= 1.15; // Increase by 15%
+        return this.hourlyPay;
+    }
+
+    // Method to calculate weekly pay
+    public double payDay(double hoursWorked) {
+        double regularHours = Math.min(hoursWorked, 40);
+        double overtimeHours = Math.max(0, hoursWorked - 40);
+        return (regularHours * hourlyPay) + (overtimeHours * hourlyPay * 1.5);
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "Name: %s %s\nThey are %.1f' %.1f\"\nThey make $%.2f\nThey have the employee ID %d\n",
+                getFirstName(), getLastName(), Math.floor(getHeight() / 12), getHeight() % 12, hourlyPay, id
+        );
+    }
 }
